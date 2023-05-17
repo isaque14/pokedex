ec138fd036dcc1de8e6487bdb32369212e86c7d5
isaque14 <isaquediniz14@gmail.com>
2023-05-17 20:25:04 -0300

    feat(regionconfiguration): configuração da Entidade Region no banco com o FluentAPI

---
+++ Pokedex.Infra.Data/EntityConfiguration/RegionConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pokedex.Domain.Entities;
+
+namespace Pokedex.Infra.Data.EntityConfiguration
+{
+    public class RegionConfiguration : IEntityTypeConfiguration<Region>
+    {
+        public void Configure(EntityTypeBuilder<Region> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(25);
+        }
+    }
+}
