63271831ef0f6d6e39b0ffbc37e4075857b193c1
isaque14 <isaquediniz14@gmail.com>
2023-05-17 20:08:22 -0300

    feat(datacontext): configuração do DataContext do Projeto

---
+++ Pokedex.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pokedex.Domain.Entities;
+using Pokedex.Infra.Data.Identity;
+
+namespace Pokedex.Infra.Data.Context
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
+    }
+}
---
+++ Pokedex.Infra.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Pokedex.Infra.Data.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
