3e691ed5d0534269957352a1e97caf130e1437e7
isaque14 <isaquediniz14@gmail.com>
2023-05-19 19:55:19 -0300

    feat(ipokeexternalapiservice): definição do serviço de consumo da API externa
    Definição do serviço que consome a api externa com o Refit

---
+++ Pokedex.Application/DTOs/PokeDataExternalAPIDTO.cs
@@ -0,0 +1,36 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+public class Abilities
+{
+    public List<string> normal { get; set; }
+    public List<string> hidden { get; set; }
+}
+
+public class Family
+{
+    public int id { get; set; }
+    public int evolutionStage { get; set; }
+    public List<string> evolutionLine { get; set; }
+}
+
+public class ExternalPoke
+{
+    public string number { get; set; }
+    public string name { get; set; }
+    public string species { get; set; }
+    public List<string> types { get; set; }
+    public Abilities abilities { get; set; }
+    public List<string> eggGroups { get; set; }
+    public List<double> gender { get; set; }
+    public string height { get; set; }
+    public string weight { get; set; }
+    public Family family { get; set; }
+    public bool starter { get; set; }
+    public bool legendary { get; set; }
+    public bool mythical { get; set; }
+    public bool ultraBeast { get; set; }
+    public bool mega { get; set; }
+    public int gen { get; set; }
+    public string sprite { get; set; }
+    public string description { get; set; }
+}
+
---
+++ Pokedex.Application/Services/ExternalAPI/IPokeExternalAPIService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+
+namespace Pokedex.Application.Services.ExternalAPI
+{
+    public interface IPokeExternalAPIService
+    {
+        [Get("pokemon/{id}")]
+        [Headers("User-Agent: PokedexAPI (https://github.com/isaque14/pokedex, v1)")]
+        Task<ExternalPoke> GetPokemonByNumberPokedex(int pokemonNumber);
+    }
+}
