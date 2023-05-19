ed624fdf1636dc68243f793268010f727debd1c6
isaque14 <isaquediniz14@gmail.com>
2023-05-19 20:45:03 -0300

    feat(mappings): mapeamentos de DTOs para entidades

---
+++ Pokedex.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Pokedex.Application.DTOs;
+using Pokedex.Domain.Entities;
+
+namespace Pokedex.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Pokemon, PokemonDTO>().ReverseMap();
+            CreateMap<Region, RegionDTO>().ReverseMap();
+
+            CreateMap<Pokemon, ExternalPoke>().ReverseMap();
+        }
+    }
+}
