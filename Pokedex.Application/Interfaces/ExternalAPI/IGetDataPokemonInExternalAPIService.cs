a3f21581dae5642f67d71faa238d289182f7c46b
isaque14 <isaquediniz14@gmail.com>
2023-05-19 20:23:00 -0300

    feat(igetdatapokemoninexternalapiservice): get dos dados externos com esta implementação

---
+++ Pokedex.Application/Interfaces/ExternalAPI/IGetDataPokemonInExternalAPIService.cs
@@ -0,0 +1,9 @@
+﻿using Pokedex.Domain.Entities;
+
+namespace Pokedex.Application.Interfaces.ExternalAPI
+{
+    public interface IGetDataPokemonInExternalAPIService
+    {
+        public Task<Pokemon> GetPokeInExternalAPIByNumberPokedexAsync(int id);
+    }
+}
---
+++ Pokedex.Application/Services/ExternalAPI/GetDataPokemonInExternalAPIService.cs
@@ -0,0 +1,13 @@
+﻿using Pokedex.Application.Interfaces.ExternalAPI;
+using Pokedex.Domain.Entities;
+
+namespace Pokedex.Application.Services.ExternalAPI
+{
+    public class GetDataPokemonInExternalAPIService : IGetDataPokemonInExternalAPIService
+    {
+        public Task<Pokemon> GetPokeInExternalAPIByNumberPokedexAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
