8ab65464f08abaea2a86b7ffa3a66ca2ad911535
isaque14 <isaquediniz14@gmail.com>
2023-05-18 21:27:11 -0300

    feat(dependencyinjectionapi.cs): criado o container de injeção de dependências do projeto

---
+++ Pokedex.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pokedex.Domain.Interfaces;
+using Pokedex.Infra.Data.Context;
+using Pokedex.Infra.Data.Identity;
+using Pokedex.Infra.Data.Repositories;
+
+namespace Pokedex.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("Default"),
+                b => b.MigrationsAssembly(typeof(DataContext).Assembly.FullName))
+            );
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<DataContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IPokemonRepository, PokemonRepository>();
+            services.AddScoped<IRegionRepository, RegionRepository>();
+            return services;
+        }
+    }
+}
