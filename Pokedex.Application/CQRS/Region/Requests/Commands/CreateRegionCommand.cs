0568bc5f303b9649ff6ef86faccf76febb3093d4
isaque14 <isaquediniz14@gmail.com>
2023-05-20 19:52:16 -0300

    feat(cqrs command): commands de Create e Update de Region

---
+++ Pokedex.Application/CQRS/Region/Requests/Commands/CreateRegionCommand.cs
@@ -0,0 +1,15 @@
+﻿using FandomStarWars.Application.CQRS.BaseResponses;
+using MediatR;
+
+namespace Pokedex.Application.CQRS.Region.Requests.Commands
+{
+    public class CreateRegionCommand : IRequest<GenericResponse>
+    {
+        public string Name { get; set; }
+
+        public CreateRegionCommand(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Pokedex.Application/CQRS/Region/Requests/Commands/UpdateRegionCommand.cs
@@ -0,0 +1,15 @@
+﻿using FandomStarWars.Application.CQRS.BaseResponses;
+using MediatR;
+
+namespace Pokedex.Application.CQRS.Region.Requests.Commands
+{
+    public class UpdateRegionCommand : IRequest<GenericResponse>
+    {
+        public int Id { get; set; }
+
+        public UpdateRegionCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
