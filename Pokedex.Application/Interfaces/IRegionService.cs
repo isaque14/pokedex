2ee36a98abe2bc213443b282b32b59cbba89fa3c
isaque14 <isaquediniz14@gmail.com>
2023-05-21 15:05:07 -0300

    feat(regionservice): interface e Implementação do Serviço de Região

---
+++ Pokedex.Application/Interfaces/IRegionService.cs
@@ -0,0 +1,11 @@
+﻿using FandomStarWars.Application.CQRS.BaseResponses;
+using Pokedex.Application.DTOs;
+
+namespace Pokedex.Application.Interfaces
+{
+    public interface IRegionService 
+    {
+        Task<GenericResponse> CreateRegioAsync(RegionDTO regionDTO);
+        Task<GenericResponse> UpdateRegioAsync(RegionDTO regionDTO);
+    }
+}
---
+++ Pokedex.Application/Services/RegionService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using FandomStarWars.Application.CQRS.BaseResponses;
+using MediatR;
+using Pokedex.Application.CQRS.Region.Requests.Commands;
+using Pokedex.Application.DTOs;
+using Pokedex.Application.Interfaces;
+
+namespace Pokedex.Application.Services
+{
+    public class RegionService : IRegionService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public RegionService(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public async Task<GenericResponse> CreateRegioAsync(RegionDTO regionDTO)
+        {
+            var createRegionCommand = _mapper.Map<CreateRegionCommand>(regionDTO);
+            var response = await _mediator.Send(createRegionCommand);
+            return response;
+        }
+
+        public async Task<GenericResponse> UpdateRegioAsync(RegionDTO regionDTO)
+        {
+            var updateRegionCommand = _mapper.Map<UpdateRegionCommand>(regionDTO);
+            var response = await _mediator.Send(updateRegionCommand);
+            return response;
+        }
+    }
+}
