d9c226bc91386c5763f23b7f44f86795946a800d
isaque14 <isaquediniz14@gmail.com>
2023-05-21 23:37:30 -0300

    feat(fakeregionrepository): repositório Fake Para utilização nos testes unitários

---
+++ Pokedex.Tests/Repositories/FakeRegionRepository.cs
@@ -0,0 +1,38 @@
+﻿using Pokedex.Domain.Entities;
+using Pokedex.Domain.Interfaces;
+
+namespace Pokedex.Tests.Repositories
+{
+    public class FakeRegionRepository : IRegionRepository
+    {
+        public async Task CreateAsync(Region region)
+        {
+            
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+           
+        }
+
+        public Task<IEnumerable<Region>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Region> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Region> GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Region region)
+        {
+            
+        }
+    }
+}
