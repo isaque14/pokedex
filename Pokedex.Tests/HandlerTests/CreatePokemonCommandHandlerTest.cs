ef8c1eb47862da9389bf8091b76c6ad9169c8309
isaque14 <isaquediniz14@gmail.com>
2023-05-26 14:44:55 -0300

    feat(fakepokemonservice): service Fake para usar nos testes de unidade

---
+++ Pokedex.Tests/HandlerTests/CreatePokemonCommandHandlerTest.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using FandomStarWars.Application.CQRS.BaseResponses;
+using Microsoft.Extensions.DependencyInjection;
+using Pokedex.Application.CQRS.Pokemons.Handlers.Commands;
+using Pokedex.Application.CQRS.Pokemons.Requests.Commands;
+using Pokedex.Application.CQRS.Pokemons.Validations;
+using Pokedex.Application.DTOs;
+using Pokedex.Application.Interfaces;
+using Pokedex.Application.Mappings;
+using Pokedex.Application.Services;
+using Pokedex.Domain.Entities.Enums;
+using Pokedex.Tests.Repositories;
+
+namespace Pokedex.Tests.HandlerTests
+{
+    public class CreatePokemonCommandHandlerTest
+    {
+        private readonly CreatePokemonCommandRequest _validCommand = new CreatePokemonCommandRequest
+        {
+            Name = "Mew",
+            Type = new List<EPokemonType>() { EPokemonType.Psychic },
+            PokedexNumber = 151,
+            Description = "Mythical Mew Pokemon",
+            EvolutionStage = 1,
+            EvolutionLine = new List<string>(),
+            IsStarter = false,
+            IsLegendary = false,
+            IsMega = false,
+            IsMythical = true,
+            IsUltraBeast = false,
+            UrlImage = "URL Image test",
+            RegionName = "Kanto",
+            RegionDTO = new RegionDTO { Id = 1, Name = "Kanto" }
+        };
+
+        private readonly CreatePokemonCommandRequest _invalidCommand = new CreatePokemonCommandRequest
+        {
+            Name = "P",
+            Type = new List<EPokemonType>() { EPokemonType.Psychic },
+            PokedexNumber = 0,
+            Description = "Mythical Mew Pokemon",
+            EvolutionStage = 1,
+            EvolutionLine = new List<string>(),
+            IsStarter = false,
+            IsLegendary = false,
+            IsMega = false,
+            IsMythical = true,
+            IsUltraBeast = false,
+            UrlImage = "URL Image test",
+            RegionName = "Kanto",
+            RegionDTO = new RegionDTO { Id = 1, Name = "Kanto" }
+        };
+
+        private readonly IMapper _mapper;
+        private readonly ValidateCreatePokemon _validator;
+        private readonly CreatePokemonCommandHandler _handler;
+
+        public CreatePokemonCommandHandlerTest()
+        {
+            var configuration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<DomainToDTOMappingProfile>();
+            });
+
+            _mapper = configuration.CreateMapper();
+            var service = new ServiceCollection();
+            service.AddSingleton(_mapper);
+            service.AddScoped<ValidateCreatePokemon>();
+            service.AddScoped<IPokemonService, PokemonService>();
+            var provider = service.BuildServiceProvider();
+            _validator = provider.GetService<ValidateCreatePokemon>();
+
+            _handler = new CreatePokemonCommandHandler(new FakePokemonRepository(), _mapper, new FakePersonageService(), _validator);
+        }
+
+        [Fact(DisplayName = "Handler with invalid command result in stop application")]
+        public void CreateMovieHandler_WithInvalidCommand_ResultInStopApplication()
+        {
+            GenericResponse response = _handler.Handle(_invalidCommand, new CancellationToken()).Result;
+            Assert.False(response.IsSuccessful);
+        }
+
+        [Fact(DisplayName = "Handler with valid command, create Movie")]
+        public void CreateMovieHandler_WithValidCommand_ResultInMovieCreated()
+        {
+            GenericResponse response = _handler.Handle(_validCommand, new CancellationToken()).Result;
+            Assert.True(response.IsSuccessful);
+        }
+    }
+}
---
+++ Pokedex.Tests/Services/FakePokemonService.cs
@@ -0,0 +1,91 @@
+﻿using FandomStarWars.Application.CQRS.BaseResponses;
+using Pokedex.Application.DTOs;
+using Pokedex.Application.Interfaces;
+
+namespace Pokedex.Tests.Services
+{
+    public class FakePokemonService : IPokemonService
+    {
+        public Task<GenericResponse> CreateAsync(PokemonDTO pokemonDTO)
+        {
+            return Task.FromResult(new GenericResponse
+            {
+                IsSuccessful = true,
+                Message = "Unitary Tests",
+                Object = pokemonDTO
+            });
+        }
+
+        public Task<GenericResponse> DeleteAsync(int id)
+        {
+            return Task.FromResult(new GenericResponse
+            {
+                IsSuccessful = true,
+                Message = "Unitary Tests"
+            });
+        }
+
+        public Task<GenericResponse> GetAllAsync()
+        {
+            return Task.FromResult(new GenericResponse
+            {
+                IsSuccessful = true,
+                Message = "Unitary Tests"
+            });
+        }
+
+        public Task<GenericResponse> GetByIdAsync(int id)
+        {
+            return Task.FromResult(new GenericResponse
+            {
+                IsSuccessful = true,
+                Message = "Unitary Tests"
+            });
+        }
+
+        public Task<GenericResponse> GetByLegendaryAsync()
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+
+        public Task<GenericResponse> GetbyMegaAsync()
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+
+        public Task<GenericResponse> GetByMythicalAsync()   
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+
+        public Task<GenericResponse> GetByNameAsync(string name)
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });    
+        }
+
+        public Task<GenericResponse> GetByPokemonNumber(int pokemonNumber)
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+
+        public Task<GenericResponse> GetByRegionNameAsync(string regionName)
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+
+        public Task<GenericResponse> GetByStarterAsync()
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+
+        public Task<GenericResponse> GetByTypeAsync(string type)
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+
+        public Task<GenericResponse> UpdateAsync(PokemonDTO pokemonDTO)
+        {
+            return Task.FromResult(new GenericResponse { IsSuccessful = true, Message = "Unitary Tests" });
+        }
+    }
+}
