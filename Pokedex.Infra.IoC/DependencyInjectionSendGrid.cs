374d555c401e3a3429ad839deed16b24f30a31c7
isaque14 <isaquediniz14@gmail.com>
2023-05-30 11:23:27 -0300

    feat(dependencyinjectionsendgrid): container de Injeção de Dependência do SendGrid

---
+++ Pokedex.Infra.IoC/DependencyInjectionSendGrid.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SendGrid.Extensions.DependencyInjection;
+
+namespace Pokedex.Infra.IoC
+{
+    public static class DependencyInjectionSendGrid
+    {
+        public static IServiceCollection AddInfrastructureSendGrid(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddSendGrid(opt =>
+            {
+                opt.ApiKey = configuration.GetSection("SendGridEmailSettings").GetValue<string>("APIKey");
+            });
+
+            return services;
+        }
+    }
+}
