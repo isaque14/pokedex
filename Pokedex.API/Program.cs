3080cdaf30a824fbd8db05405e074d47039891cd
isaque14 <isaquediniz14@gmail.com>
2023-05-30 15:04:11 -0300

    style(program.cs): alteração do nome da Aplicação do Swagger

--- Pokedex.API/Program.cs
+++ Pokedex.API/Program.cs
@@ -33,7 +33,7 @@ app.UseSwagger();
 
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Famdom Star Wars API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PokeDex API V1");
     c.RoutePrefix = "swagger";
 });
 
