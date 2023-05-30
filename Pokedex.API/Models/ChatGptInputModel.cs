3b439005c117bf46eb65d189143786d7e3151ebd
isaque14 <isaquediniz14@gmail.com>
2023-05-30 12:45:06 -0300

    feat(chatgptinputmodel): inputModel para o ChatGPT

---
+++ Pokedex.API/Models/ChatGptInputModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Pokedex.API.Models
+{
+    public class ChatGptInputModel
+    {
+        public string model { get; set; }
+        public string prompt { get; set; }
+        public int max_tokens { get; set; }
+        public decimal temperature { get; set; }
+
+        public ChatGptInputModel(string prompt)
+        {
+            this.prompt = $"pokémon: {prompt}";
+            temperature = 0.2m;
+            max_tokens = 100;
+            model = "text-davinci-003";
+        }
+    }
+}
