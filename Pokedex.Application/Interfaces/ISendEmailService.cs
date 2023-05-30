d9850415f895cb3f59f695e0b2d901f8000ba745
isaque14 <isaquediniz14@gmail.com>
2023-05-30 11:17:14 -0300

    feat(sendemailservice): interface e Implementação do serviço de envio de e-mail

---
+++ Pokedex.Application/Interfaces/ISendEmailService.cs
@@ -0,0 +1,9 @@
+﻿using FandomStarWars.Application.CQRS.BaseResponses;
+
+namespace Pokedex.Application.Interfaces
+{
+    public interface ISendEmailService
+    {
+        Task<GenericResponse> SendEmail(string email, string subject, string content);
+    }
+}
---
+++ Pokedex.Application/Services/SendEmailService.cs
@@ -0,0 +1,53 @@
+﻿using FandomStarWars.Application.CQRS.BaseResponses;
+using Microsoft.Extensions.Configuration;
+using Pokedex.Application.Interfaces;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace Pokedex.Application.Services
+{
+    public class SendEmailService : ISendEmailService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ISendGridClient _sendGridClient;
+
+        public SendEmailService(IConfiguration configuration, ISendGridClient sendGridClient)
+        {
+            _configuration = configuration;
+            _sendGridClient = sendGridClient;
+        }
+
+        public async Task<GenericResponse> SendEmail(string email, string subject, string content)
+        {
+            string fromEmail = _configuration.GetSection("SendGridEmailSettings")
+            .GetValue<string>("FromEmail");
+
+            string fromName = _configuration.GetSection("SendGridEmailSettings")
+            .GetValue<string>("FromName");
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(fromEmail, fromName),
+                Subject = subject,
+                PlainTextContent = content
+            };
+
+            msg.AddTo(email);
+            var response = await _sendGridClient.SendEmailAsync(msg);
+
+            if (response.IsSuccessStatusCode)
+                return new GenericResponse
+                {
+                    IsSuccessful = true,
+                    Message = "Email Send Successfully"
+                };
+
+            else
+                return new GenericResponse
+                {
+                    IsSuccessful = false,
+                    Message = "Email Sending Failed"
+                };
+        }
+    }
+}
