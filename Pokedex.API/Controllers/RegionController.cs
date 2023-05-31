14aa373666140c051114208d000522833835b93d
isaque14 <isaquediniz14@gmail.com>
2023-05-30 22:06:43 -0300

    docs(regioncontroller): documentação do Controller de Região

--- Pokedex.API/Controllers/RegionController.cs
+++ Pokedex.API/Controllers/RegionController.cs
@@ -19,8 +19,18 @@ namespace Pokedex.API.Controllers
             _regionService = regionService;
         }
 
+
+        /// <summary>
+        /// Cadastrar Região
+        /// </summary>
+        /// <param name="regionName">Nome da Região</param>
+        /// <returns>Região Criada</returns>
+        /// <response code="201">Sucesso</response>
+        /// <response code="400">Falha</response>
         [HttpPost]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<PokemonDTO>> CreateRegion(string regionName)
         {
             var response = await _regionService.CreateRegioAsync(new RegionDTO { Name = regionName });
@@ -28,8 +38,22 @@ namespace Pokedex.API.Controllers
             return response.IsSuccessful ? StatusCode(201, response) : BadRequest(response);
         }
 
+        /// <summary>
+        /// Atualizar um Região
+        /// </summary>
+        /// <param name="id">Identificador da Região</param>
+        /// <param name="movie">Dados da Região</param>
+        /// <returns>Região Atualizada</returns>
+        /// <response code="404">Não Encontrado</response>
+        /// <response code="200">Sucesso</response>
+        /// <response code="403">Credencias não autorizadas</response>
+        /// <response code="401">Não Autorizado</response>
         [HttpPut]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<PokemonDTO>> UpdateRegion(int id, RegionDTO regionDTO)
         {
             if (id != regionDTO.Id || regionDTO is null)
